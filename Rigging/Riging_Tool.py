9a09e63d9e860645b0572b2e70818074d7ace195
bum <keyring.yong@gmail.com>
2018-02-01 17:33:41 +0900

    희망이 안 보입니다 ㅠㅠ

--- Rigging/Riging_Tool.py
+++ Rigging/Riging_Tool.py
@@ -1,3 +1,4 @@
+#coding:cp949
 import pymel.core as pm
 
 windowname = 'RigingTool'
@@ -59,78 +60,79 @@ class Riging():
                                           command=lambda:self.colorselct(23))  # up11 #color:23
                         pm.iconTextButton(style='iconAndTextVertical', image1='black.png',
                                           command=lambda:self.colorselct(1))  # down11 #color:1
-                        pm.setParent('..')
-                        with pm.columnLayout(adj=True):
+
+                    with pm.columnLayout(adj=True):
+                        pm.separator(h=10, style='out')
+                        with pm.frameLayout(label='Ctrl Color', backgroundColor=[0.4, 0.4, 0.4]):
                             pm.separator(h=10, style='out')
-                            with pm.frameLayout(label='Ctrl Color', backgroundColor=[0.4, 0.4, 0.4]):
+                            with pm.columnLayout(adj=True):
+                                pm.gridLayout(numberOfColumns=9, cellWidthHeight=(40, 40), width=300)
+                                # icons of the "controllers"
+                                pm.symbolButton(image='mz_icons/ccButton01.PNG', command=lambda :self.colorselct(A=True))
+                                pm.symbolButton(image='mz_icons/ccButton02.PNG', command=lambda :self.colorselct(B=True))
+                                pm.symbolButton(image='mz_icons/ccButton03.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton04.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton05.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton06.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton07.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton08.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton09.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton10.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton11.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton12.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton13.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton14.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton15.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton16.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton17.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton18.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton19.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton20.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton21.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton22.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton23.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton24.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton25.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton26.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton27.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton28.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton29.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton30.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton31.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton32.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton33.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton34.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton35.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton36.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton37.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton38.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton39.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton40.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton41.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton42.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton43.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton44.PNG')
+                                pm.symbolButton(image='mz_icons/ccButton45.PNG')
+
+                            with pm.columnLayout(adj=True):
                                 pm.separator(h=10, style='out')
-                                with pm.columnLayout(adj=True):
-                                    pm.gridLayout(numberOfColumns=9, cellWidthHeight=(40, 40), width=300)
-                                    # icons of the "controllers"
-                                    pm.symbolButton(image='mz_icons/ccButton01.PNG', command=lambda:self.Ctrl_Create(A=1))
-                                    pm.symbolButton(image='mz_icons/ccButton02.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton03.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton04.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton05.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton06.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton07.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton08.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton09.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton10.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton11.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton12.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton13.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton14.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton15.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton16.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton17.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton18.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton19.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton20.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton21.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton22.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton23.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton24.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton25.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton26.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton27.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton28.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton29.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton30.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton31.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton32.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton33.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton34.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton35.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton36.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton37.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton38.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton39.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton40.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton41.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton42.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton43.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton44.PNG')
-                                    pm.symbolButton(image='mz_icons/ccButton45.PNG')
-                                    pm.setParent('..')
+                                with pm.rowColumnLayout(numberOfRows=1, rowHeight=((1, 20), (2, 20))):
+
+                                    pm.text('                    ')
+                                    pm.checkBox(label='    ')
+                                    pm.button(label='change', w=50, h=20)
+                                    pm.text('                    ')
+                                    pm.checkBox(label='    ')
+                                    pm.button(label='change', w=50, h=20)
                                     pm.separator(h=10, style='out')
 
-                                    with pm.columnLayout(adj=True):
-                                        with pm.rowColumnLayout(numberOfRows=1, rowHeight=((1, 20), (2, 20))):
-                                            pm.separator(h=10, style='out')
-                                            pm.text('                    ')
-                                            pm.checkBox(label='    ')
-                                            pm.button(label='change', w=50, h=20)
-                                            pm.text('                    ')
-                                            pm.checkBox(label='    ')
-                                            pm.button(label='change', w=50, h=20)
-                                            pm.setParent('..')
-                                            with pm.columnLayout(adj=True):
-                                                pm.separator(h=10, style='out')
+                                with pm.columnLayout(adj=True):
+                                    pm.separator(h=10, style='out')
 
     def colorselct(self, number):
+        #컨트롤러 선택
         SelectTarget = pm.ls(sl=1)
-
+        #졩트롤러 쉐입에 대한 색상 바꾸기
         for ctrl in SelectTarget:
             print ctrl
             shape = pm.listRelatives(ctrl, s=1)[0]
@@ -138,23 +140,48 @@ class Riging():
             pm.setAttr('%s.overrideEnabled' % shape, 1)
             pm.setAttr('%s.overrideColor' % shape, number)
 
-    def Ctrl_Create(self, A=None):
+    def Create(self, A=None, B=None):
+        #A가 True일때 Ctrl를 생성
+        if A == True:
 
-        if A == 1:
             pm.circle(c=(0, 0, 0), nr=(0, 1, 0))
-        # if B == True:
-        #     pm.curve(
-        #         p=[(1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, 1.011106),
-        #            (1.011106, 1.011106, 1.011106), (1.011106, 1.011106, -1.011106), (1.011106, -1.011106, -1.011106),
-        #            (-1.011106, -1.011106, -1.011106), (-1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, 1.011106),
-        #            (-1.011106, -1.011106, 1.011106), (-1.011106, -1.011106, -1.011106),
-        #            (1.011106, -1.011106, -1.011106),
-        #            (1.011106, -1.011106, 1.011106), (1.011106, 1.011106, 1.011106), (-1.011106, 1.011106, 1.011106),
-        #            (-1.011106, -1.011106, 1.011106), (1.011106, -1.011106, 1.011106)],
-        #         k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], d=1)
+
+        # B가 True일때 Ctrl를 생성
+        if B == True:
+            pm.curve(
+                p=[(1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, 1.011106),
+                   (1.011106, 1.011106, 1.011106), (1.011106, 1.011106, -1.011106), (1.011106, -1.011106, -1.011106),
+                   (-1.011106, -1.011106, -1.011106), (-1.011106, 1.011106, -1.011106), (-1.011106, 1.011106, 1.011106),
+                   (-1.011106, -1.011106, 1.011106), (-1.011106, -1.011106, -1.011106),
+                   (1.011106, -1.011106, -1.011106),
+                   (1.011106, -1.011106, 1.011106), (1.011106, 1.011106, 1.011106), (-1.011106, 1.011106, 1.011106),
+                   (-1.011106, -1.011106, 1.011106), (1.011106, -1.011106, 1.011106)],
+                k=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], d=1)
 
 
 
+
+
+
+''''
+========================================================================================
+runcode
+import sys 
+sys.path.append('Q:굚yongbum굚PYTHON_PATH굚KeyringForMaya굚Rigging')
+import Riging_Tool
+reload(Riging_Tool)
+Riging_Tool.Riging()
+
+=======================================================================================
+
+질문입니다
+1.함수가  Create() 실행이 왜 안 될까요?
+2.class 안에서 인자 값을 넣고 이수값을 넣는 방법을 잘 모르겠습니다 
+3.혹시 iconTextButton 버튼과  symbolButton 버튼의 이벤트 발생하는 스크립트 방식이 다른가요?
+
+===========================================================================================
+
+'''''
 
 
 
